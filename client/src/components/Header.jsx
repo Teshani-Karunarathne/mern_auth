5ebc31f2ec7df12a9120ff8ca8c8ab7b538cec2d
tesh_k <teshanikarunarathna@gmail.com>
2024-07-01 16:51:14 +0530

    create header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-unused-vars */
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <div className='bg-slate-200'>
+        <div className="flex justify-between item center max-w-6xl mx-auto p-3">
+            <h1 className='font-bold'>Auth App</h1>
+            <ul className='flex gap-4'>
+                <Link to='/'><li>Home</li></Link>
+                <Link to='/about'><li>About</li></Link>
+                <Link to='/sign-in'><li>Sign In</li></Link>
+            </ul>
+        </div>
+    </div>
+  )
+}
