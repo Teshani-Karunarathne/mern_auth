100c5e9e3ffc0c42644fe4e6f8e303bef17bf9dc
tesh_k <teshanikarunarathna@gmail.com>
2024-07-01 15:29:59 +0530

    add routes and create pages

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+
+export default function About() {
+  return (
+    <h1>About</h1>
+  )
+}
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+export default function SignIn() {
+  return (
+    <h1>SignIn</h1>
+  )
+}
